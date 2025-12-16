94d4eb4ac1245713482883f53cb2845ec34790d7
John Lu <johnlu1161@gmail.com>
2025-12-16 15:03:24 -0800

    Edit firebase config

--- react-app/src/firebase.js
+++ react-app/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCB2BfnRlaPkfdPQVKd_mJbQPa2eVPG_is",
-  authDomain: "restaureantf.firebaseapp.com",
-  projectId: "restaureantf",
-  storageBucket: "restaureantf.firebasestorage.app",
-  messagingSenderId: "387903781362",
-  appId: "1:387903781362:web:daaeabbb3387e34791936b",
-  measurementId: "G-3CT4CWFR83"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
